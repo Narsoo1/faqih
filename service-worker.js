5fee708109e3be7068cdb02580292ebd636b71a2
vlamodix <duriahrealmenarzo@gmail.com>
2025-07-24 09:21:33 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,49 @@
-// service-worker.js
-self.addEventListener('install', function (event) {
-  console.log('Service Worker installing.');
+const CACHE_NAME = "Qih-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/intro.html",
+  "/verivikasi.html",
+  "/login.html",
+  "/register.html",
+  "/password.html",
+  "/reset.html",
+  "/chat.html",
+  "/faqih.png",
+  "/oke.png",
+  "/qih.svg",
+  "/manifest.json",
+];
+
+// Install service worker dan cache file
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('activate', function (event) {
-  console.log('Service Worker activating.');
+// Ambil file dari cache saat offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update cache jika ada versi baru
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
 });
